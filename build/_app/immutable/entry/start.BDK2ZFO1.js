181ddd360dc9f0b554e71010cf92ca147e9a0bc7
sgm-17 <88693771+sgm-17@users.noreply.github.com>
2026-03-11 16:50:43 +0000

    build

---
+++ build/_app/immutable/entry/start.BDK2ZFO1.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BQuKkLzM.js";import{x as t}from"../chunks/BfIEWp77.js";export{t as load_css,r as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { onMount, onDestroy } from "svelte";
   import { base } from '$app/paths';
-
+  
   import panzoom from "panzoom";
 
   let showPopup = true;
